3552afc206093471a9b53bc8714d94fae71e9d26
CarlaJay <carlajaygersamio081@gmail.com>
2024-12-10 04:03:43 +0800

    Create a Book Model and Migration

--- database/migrations/2024_12_09_193357_create_books_table.php
+++ database/migrations/2024_12_09_193357_create_books_table.php
@@ -9,13 +9,16 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('books', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+    public function up()
+{
+    Schema::create('books', function (Blueprint $table) {
+        $table->id();
+        $table->string('title');
+        $table->string('author');
+        $table->integer('quantity');
+        $table->timestamps();
+    });
+}
 
     /**
      * Reverse the migrations.
